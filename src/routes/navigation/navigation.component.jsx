5fa01e877fc304ab1b2506bdd6d8fc4ac6f5969d
Justin Liman <justinliman97@gmail.com>
2022-07-29 09:38:10 -0700

    add routing and navbar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CowLogo } from '../../assets/cow-svgrepo-com.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CowLogo className='logo' width='50px' height='39px' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
