c4afa61cd0d116f3825e9aedc161cd172737a45d
Justin Liman <justinliman97@gmail.com>
2022-10-31 11:53:37 -0700

    implement function to get item categories from firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,28 @@
 import { createContext, useState, useEffect } from "react";
-import PRODUCTS from '../shop-data.json';
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+// import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
   products: []
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+  // One time setting data in db
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // }, []);
+
   const value = {products};
 
   
