1f1dac9b64d69ab9712b55cb6828179ba63057f8
Justin Liman <justinliman97@gmail.com>
2022-11-03 20:01:02 -0700

    shop page displays different categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
 
 import ProductCard from "../../components/product-card/product-card.component";
@@ -9,13 +9,22 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   // console.log(SHOP_DATA);
   return (
-    <div className='products-container'>
-      {/* {products.map((product) => {
-        return (
-          <ProductCard key={product.id} product={product} />
-        ); */}
-      })}
-    </div>
+    <Fragment>
+      {
+        Object.keys(categoriesMap).map(title => (
+          <Fragment key={title}>
+            <h2>{title}</h2>
+            <div className='products-container'>
+              {categoriesMap[title].map((product) => {
+                return (
+                  <ProductCard key={product.id} product={product} />
+                );
+              })}
+            </div>
+          </Fragment>
+        ))
+      }
+    </Fragment>
   );
 };
 
