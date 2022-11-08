439134d0600e4854fb637fa7cc205b4326a16674
Justin Liman <justinliman97@gmail.com>
2022-11-08 02:38:44 -0800

    start to convert to styled components

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,6 +1,6 @@
 // import { useEffect } from 'react';
 // import { getRedirectResult } from 'firebase/auth';
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+// import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,7 +11,9 @@ import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import './navigation.styles.scss';
+import { NavigationComponent } from './navigation.styles';
+
+// import './navigation.styles.scss';
 
 const Navigation = () => {
   console.log('rendering navbar');
