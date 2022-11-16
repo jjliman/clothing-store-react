ad96fd4a77f37897d171d92b0653f244e088bdbe
Justin Liman <justinliman97@gmail.com>
2022-11-16 02:28:00 -0800

    add redux store provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
+
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
@@ -13,6 +16,7 @@ import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
@@ -22,6 +26,7 @@ root.render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
+  </Provider>
   // </React.StrictMode>
 );
 
